cbdbcd30fe345fc066cf347beb24c53eb1ca873e
S-Romeaux <sal.4845@gmail.com>
2023-09-13 02:07:19 -0700

    Finished code changes

--- public/index.js
+++ public/index.js
@@ -1,9 +1,68 @@
-async function main() {
-
-    const timeChartCanvas = document.querySelector('#time-chart');
-    const highestPriceChartCanvas = document.querySelector('#highest-price-chart');
-    const averagePriceChartCanvas = document.querySelector('#average-price-chart');
-
+function getColor(stock) {
+    if (stock === "GME") {return "rgba(61, 161, 61, 0.7)";}
+    if (stock === "MSFT") {return "rgba(209, 4, 25, 0.7)";}
+    if (stock === "DIS") {return "rgba(18, 4, 209, 0.7)";}
+    if (stock === "BNTX") {
+        return "rgba(166, 43, 158, 0.7)";
+    }
 }
 
-main()
+async function main() {
+    const timeChartCanvas = document.querySelector("#time-chart");
+    const highestPriceChartCanvas = document.querySelector("#highest-price-chart");
+    const averagePriceChartCanvas = document.querySelector("#average-price-chart");
+    const { GME, MSFT, DIS, BNTX } = mockData;
+    const stocks = [GME, MSFT, DIS, BNTX];
+    stocks.forEach((stock) => stock.values.reverse());
+
+    new Chart(timeChartCanvas.getContext("2d"), {
+        type: "line",
+        data: {
+            labels: stocks[0].values.map((value) => value.datetime),
+            datasets: stocks.map((stock) => ({
+                label: stock.meta.symbol,
+                data: stock.values.map((value) => parseFloat(value.high)),
+                backgroundColor: getColor(stock.meta.symbol),
+                borderColor: getColor(stock.meta.symbol),
+            })),
+        },
+    });
+
+    new Chart(highestPriceChartCanvas.getContext("2d"), {
+        type: "bar",
+        data: {
+            labels: stocks.map((stock) => stock.meta.symbol),
+            datasets: [
+                {
+                    label: "Highest",
+                    backgroundColor: stocks.map((stock) => getColor(stock.meta.symbol)),
+                    borderColor: stocks.map((stock) => getColor(stock.meta.symbol)),
+                    data: stocks.map((stock) => findHighest(stock.values)),
+                },
+            ],
+        },
+    });
+}
+
+function findHighest(values) {
+    let highest = 0;
+    values.forEach((value) => {
+        if (parseFloat(value.high) > highest) {
+            highest = parseFloat(value.high);
+        }
+    });
+    return highest;
+}
+
+main();
+
+
+
+// Just to pull stocks
+// async function main() {
+//     const response = await fetch(`https://api.twelvedata.com/time_series?symbol=GME,MSFT,DIS,BNTX&interval=1day&apikey=89139e697308459d9159050587435911`)
+//     const result = await response.json()
+//     const { GME, MSFT, DIS, BNTX } = result;
+//     const stocks = [GME, MSFT, DIS, BNTX];
+// }
+// main()
